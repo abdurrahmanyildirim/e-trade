2aff46b5b8fe81f8ffde4321abb0c794a4ea9437
Abdurrahman Yıldırım <abdurrahman.yildirim@matriksdata.com>
2021-12-23 22:42:20 +0300

    Trust words eklendi.

--- front-end/src/app/pages/main/module.ts
+++ front-end/src/app/pages/main/module.ts
@@ -4,11 +4,12 @@ import { CardModule } from 'src/app/shared/components/card/module';
 import { MainComponent } from './component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [{ path: '', component: MainComponent }];
 @NgModule({
   declarations: [MainComponent],
-  imports: [RouterModule.forChild(routes), CommonModule, CardModule, CarouselModule],
+  imports: [RouterModule.forChild(routes), CommonModule, CardModule, CarouselModule, MatIconModule],
   exports: [MainComponent]
 })
 export class MainModule {}
