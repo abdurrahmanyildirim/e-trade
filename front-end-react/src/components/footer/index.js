856305991a14efc0efd3433c62e68a22535c0cfc
Abdurrahman Yıldırım <abdurrahman.yildirim@matriksdata.com>
2023-01-05 08:21:29 +0300

    Inited Cypress. Removed react directory

--- front-end-react/src/components/footer/index.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(props) {
-    return (
-        <div>
-            Footer
-        </div>
-    )
-}
---
+++ front-end/cypress/e2e/spec-2.cy.ts
@@ -0,0 +1,11 @@
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(false);
+  });
+});
